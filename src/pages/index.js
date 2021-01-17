1b108b5d9c542d64d649b110d9f60cca81894432
Hallvard Jensen <hallvard.jensen@gmail.com>
2021-01-17 01:36:09 +0100

    Removed a dangling style attribute from Layout tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = ({ data }) => (
-  <Layout style>
+  <Layout>
     <SEO title="Lumberian Records" />
     <h1>Latest articles</h1>
     <hr />
