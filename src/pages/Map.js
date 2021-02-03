8b4f95d3aa56ba023cade916b18d8b8c1178d463
Hallvard Jensen <hallvard.jensen@gmail.com>
2021-02-03 11:46:52 +0100

    Functioning zoom on map

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,45 +1,53 @@
-import React from "react"
+import React, { Component } from "react"
 
 import LumberiaMap from "../images/map/test_map.jpg"
-
-function zoom(e) {
-  e.preventDefault()
-
-  scale += e.deltaY * -0.01
-
-  // Restrict scale
-  scale = Math.min(Math.max(0.125, scale), 4)
-
-  // Apply scale transform
-  map.style.transform = `scale(${scale})`
-}
+import "../components/Map.css"
 
 let scale = 1
-const map = document.querySelector("#map")
-map.onwheel = zoom
 
-const Map = () => {
-  return (
-    <div
-      className="mapWrapper"
-      style={{
-        minHeight: "100vh",
-        margin: "0",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <img
-        id="map"
-        src={LumberiaMap}
-        width="3492"
-        height="2480"
-        onwheel={zoom}
-        alt="Eastern Lumberia"
-      />
-    </div>
-  )
+class Map extends Component {
+  constructor(props) {
+    super(props)
+
+    this.zoom = this.zoom.bind(this)
+  }
+
+  zoom(e) {
+    e.preventDefault()
+
+    scale += e.deltaY * -0.01
+
+    // Restrict scale
+    scale = Math.min(Math.max(0.125, scale), 4)
+
+    // Apply scale transform
+    this.mapElement.style.transform = `scale(${scale})`
+  }
+
+  render() {
+    return (
+      <div
+        className="mapWrapper"
+        style={{
+          minHeight: "100vh",
+          margin: "0",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <img
+          id="map"
+          src={LumberiaMap}
+          width="3492"
+          height="2480"
+          alt="Eastern Lumberia"
+          onWheel={this.zoom}
+          ref={el => (this.mapElement = el)}
+        />
+      </div>
+    )
+  }
 }
 
 export default Map
